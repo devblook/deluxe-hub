1ff68bccd57815cc5f59737a718b2fd8ad480176
Cristóbal Veas <cristobal.veas@proton.me>
2024-12-23 01:20:35 -0300

    build: bump paperweight-userdev to 2.0.0-beta.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     id("com.gradleup.shadow") version ("8.3.5")
-    id("io.papermc.paperweight.userdev") version ("1.7.7")
+    id("io.papermc.paperweight.userdev") version ("2.0.0-beta.8")
 }
 
 group = "team.devblook"
